0dc8680f8afa4fe6f1e9f71116e9dbfd003238c6
sari <sari.korpialho@gmail.com>
2022-02-18 16:05:13 +0200

    part 2.4 ready

--- part2b/src/App.js
+++ part2b/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Person from './component/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = (props) => {
+  const [persons, setPerson] = useState(props.persons)
+  const [newPerson, setNewPerson] = useState(
+    ''
+  ) 
+
+const addPerson = (event) => {
+  event.preventDefault()
+  const personNew = {
+    name: newPerson,
+    id: persons.length + 1
+  }
+  setPerson(persons.concat(personNew))
+  setNewPerson('')
 }
 
-export default App;
+    const handlePersonChange = (event) => {
+      console.log(event.target.value)
+      setNewPerson(event.target.value)
+    }
+ 
+
+  
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={addPerson}>
+          <div>
+            nimi:
+        <input
+          value={newPerson}
+          onChange={handlePersonChange}
+        />
+        </div>
+        <div>
+        <button type="submit">lisää</button>
+        </div>
+      </form> 
+        <h2>Numerot</h2>
+        <ul>
+          {persons.map(person => <Person key={person.id} person={person} />)}
+        </ul>
+      </div>
+    )
+  
+}
+
+export default App
+
--- part2b/src/index.js
+++ part2b/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import App from './App.js'
+
+const persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040'
+  },
+  {
+    id: 2,
+    name: 'Taina',
+    number: '050'
+  }
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App persons={persons} />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
